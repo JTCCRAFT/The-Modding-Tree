8582a90e705b563840635461e14bf14f888061fc
JTCCRAFT <jctcraft@gmail.com>
2026-03-25 20:51:53 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("p", {
-    name: "Pain", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("t", {
+    name: "Tokens", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "#f0000",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Pain", // Name of prestige currency
-    baseResource: "Tears", // Name of resource prestige is based on
+    resource: "Tokens", // Name of prestige currency
+    baseResource: "Dice", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,13 +22,13 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Sacrifice tears for pain", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "t", description: "T: Gamble for tokens", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 	    upgrades: {
             11: {
-                title: "Wet eyes",
-                description: "2x more tears",
+                title: "Rigged",
+                description: "Rig the gamble to get 2x times more tokens",
                 cost: new Decimal(1),
         },
     },
