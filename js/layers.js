4ac98238ddef82f31a4c83d8402cb59262854ab0
JTCCRAFT <jctcraft@gmail.com>
2026-03-25 20:43:39 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,10 +6,10 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#f0000",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Pain", // Name of prestige currency
+    baseResource: "Tears", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
